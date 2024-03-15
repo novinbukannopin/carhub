b33fcfd421565d7bc7e74c59ab437918d49801c6
novinbukannopin <novinardiany@outlook.com>
2024-03-15 11:07:11 +0700

    :truck: feat<home/catalog> : add dynamic search bar

--- components/index.tsx
+++ components/index.tsx
@@ -2,10 +2,16 @@ import Hero from "@/components/Hero";
 import CustomButton from "@/components/CustomButton";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
     Hero,
     Navbar,
     Footer,
-    CustomButton
+    CustomButton,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
 }
