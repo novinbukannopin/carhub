75a6fc6ef37f580c0835b0579aee58297e51a3de
novinbukannopin <novinardiany@outlook.com>
2024-03-15 10:25:46 +0700

    :zap: feat<home> : add page

---
+++ components/index.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+import CustomButton from "@/components/CustomButton";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export {
+    Hero,
+    Navbar,
+    Footer,
+    CustomButton
+}
